5f7ca5b056524f1b0dd7ddc09d09a1ceb4cd3059
HoangWhites <hoangnamwhiteshub@gmail.com>
2024-10-25 21:04:04 +0700

    Update BloxFruitMain.lua

--- BloxFruitMain.lua
+++ BloxFruitMain.lua
@@ -1,1 +1,517 @@
 
+(
+[
+[This file was protected with MoonSec V3]]) : gsub('.+',  (function(a) _cyhjfhfuPupM =  a;
+ end));
+
+return (function(o, .) local r; local f; local t; local a; local d; local s; local e = 24915; local n = 0; local l = {};
+while n < 147 do n = n + 1;
+while n < 0xa0 and e % 0xb78 < 0x5bc do n = n + 1 e = (e * 589) % 26243 local h = n + e
+if (e % 0xaf8) < 0x57c then e = (e + 0xa3) % 0x63cd
+while n < 0x2b1 and e % 0xc70 < 0x638 do n = n + 1 e = (e * 66) % 45657 local t = n + e
+if (e % 0x2e7e) > 0x173f then e = (e - 0x301) % 0x8638 local e = 86702
+if not l[e] then l[e] = 0x1 a = "\4\8\116\111\110\117\109\98\101\114\95\118\67\106\105\116\104\84\0\6\115\116\114\105\110\103\4\99\104\97\114\69\122\113\86\68\111\82\72\0\6\115\116\114\105\110\103\3\115\117\98\82\87\76\66\106\99\97\104\0\6\115\116\114\105\110\103\4\98\121\116\101\117\108\121\102\102\109\109\88\0\5\116\97\98\108\101\6\99\111\110\99\97\116\108\81\74\116\76\107\112\88\0\5\116\97\98\108\101\6\105\110\115\101\114\116\105\85\68\110\119\88\100\98\5"; end elseif e % 2~ = 0 then e = (e - 0x2f4) % 0x17d local e = 35636
+if not l[e] then l[e] = 0x1 d = (not d) and _ENV or d; end
+else e = (e * 0x2e4) % 0x71ef n = n + 1 local e = 86842
+if not l[e] then l[e] = 0x1 s = tonumber; end end end elseif e % 2~ = 0 then e = (e + 0x292) % 0x804e
+while n < 0x39f and e % 0x4430 < 0x2218 do n = n + 1 e = (e - 928) % 37774 local a = n + e
+if (e % 0x1162) >= 0x8b1 then e = (e + 0x2f8) % 0x743c local e = 25733
+if not l[e] then l[e] = 0x1 t = function(l) local e = 0x01 local function n(n) e = e + n
+return l: sub(e - n, e - 0x01) end
+while true do local l = n(0x01) if (l == "\5") then
+break end local e = f.byte(n(0x01)) local e = n(e) if l == "\2"
+then e = r._vCjithT(e) elseif l == "\3"
+then e = e~ = "\0"
+elseif l == "\6"
+then d[e] = function(e, n) return o(8, nil, o, n, e) end elseif l == "\4"
+then e = d[e] elseif l == "\0"
+then e = d[e]
+[n(f.byte(n(0x01)))]; end local n = n(0x08) r[n] = e end end end elseif e % 2~ = 0 then e = (e * 0x123) % 0xa89c local e = 81747
+if not l[e] then l[e] = 0x1 end
+else e = (e + 0x37a) % 0x6fe3 n = n + 1 local e = 60039
+if not l[e] then l[e] = 0x1 end end end
+else e = (e * 0x1fe) % 0x7a41 n = n + 1
+while n < 0x280 and e % 0x27f4 < 0x13fa do n = n + 1 e = (e * 869) % 33985 local t = n + e
+if (e % 0xdf4) >= 0x6fa then e = (e - 0x31) % 0x1fea local e = 88366
+if not l[e] then l[e] = 0x1 d = getfenv and getfenv(); end elseif e % 2~ = 0 then e = (e - 0x3f4) % 0x1ddf local e = 66053
+if not l[e] then l[e] = 0x1 f = string; end
+else e = (e + 0x3d8) % 0x3955 n = n + 1 local e = 45486
+if not l[e] then l[e] = 0x1 r = {}; end end end end end e = (e + 158) % 20219 end t(a); local e = {};
+for n = 0x0, 0xff do local l = r.EzqVDoRH(n); e[n] = l; e[l] = n; end local function h(n) return e[n]; end local a = (function(a, t) local f, l = 0x01, 0x10 local n = {
+    {},
+    {},
+    {}
+}
+local d = -0x01 local e = 0x01 local o = a
+while true do n[0x03]
+[r.RWLBjcah(t, e, (function() e = f + e
+return e - 0x01 end)
+())] = (function() d = d + 0x01
+return d end)
+() if d == (0x0f) then d = ""
+l = 0x000
+break end end local d = #t
+while e < d + 0x01 do n[0x02]
+[l] = r.RWLBjcah(t, e, (function() e = f + e
+return e - 0x01 end)
+()) l = l + 0x01
+if l % 0x02 == 0x00 then l = 0x00 r.iUDnwXdb(n[0x01], (h(
+(
+(
+(n[0x03]
+[n[0x02]
+[0x00]] or 0x00) * 0x10) + (n[0x03]
+[n[0x02]
+[0x01]] or 0x00) + o) % 0x100))); o = a + o; end end
+return r.lQJtLkpX(n[0x01]) end); t(a(229, ".(sJG1eLq<ytl9xFs_")); t(a(94, "c*k69)wEdAWHr_ThWk9W)AhrAr6)_rEEHwHdwHv)WW6*r9w)6HHE9T*WWhhDA6W{6)_)dkk9Wr6hh_A9)d_jEhkhwH***Trk)H^kWW*hT9dA**rHw*h9A6TdA4Ah9kTkEHkkH_h_,EA_)kT6dW6%HTw_*dW6w6_*AT_9EWd)*H_FwwW)WT9dTHW}6WrTd9hdA)w_hTH-6Ad4kW66rEEd*6W_)TT_Wk6*_9dY*Hr*E6TdWJkdrhwWJ_wr**okH6)6lwW69dWHdW8_Ahw6*{H*9_rAkT_;T)E**Hrd6rTkd?k6h)Eh66_6JdWAd66Hb)Wd6_Tdw_LWWE/PWwH9)*hEAW6W_6EW*)AH)6R*dW96HT)_*)w!vE*dWE9WhwA*_9T)d+k9HE)HS)Ay9*hAd66*W_EkY9)%h9X)WJ99_EEHk)HQ9Q*6W_)E__Ar6HE1*6k#r9)_;9Wr9Trwd)6WWhE04THETTAkW*6)_TEA*Arrww0dA*))hWE96r_d7WH5HH)*/wAr_H_TdwkHr_Ekh6AA9)_9AhITHddwHE")); local e = (-1171 + (function() local t, e = 0, 1;
+(function(e, n, l) n(e(e, l, n), n(e, e, n), l(n, e and n, e)) end)
+(function(l, n, d) if t > 263 then
+return d end t = t + 1 e = (e - 149) % 16744
+if (e % 1962) >= 981 then
+return n
+else
+return l(l(l, n, n), n(n and d, n, n), l(d, n, l)) end
+return d(n(d, n, l), d(d, d, l), d(n, l, l)) end, function(l, n, d) if t > 166 then
+return d end t = t + 1 e = (e * 637) % 6683
+if (e % 556) <= 278 then
+return l(l(l, n, d), n(n, n, l), l(n and d, n, d and n) and d(l, n, n))
+else
+return n end
+return l end, function(n, l, d) if t > 378 then
+return d end t = t + 1 e = (e * 725) % 35709
+if (e % 1746) <= 873 then e = (e * 71) % 7512
+return n(d(l, l, n), l(n, d and n, d), l(n, n, n))
+else
+return l end
+return n end) return e; end)
+()) local le = (getfenv) or(function() return _ENV end); local c = r.TBsksWtq or r.TcyamGjZ; local ne = 1; local d = 2; local t = 3; local f = 4; local function y(m, .) local h = a(e, "d&av7e.-z,?1Ehm vze.z&?Ehz* ahezzvihvE-h,-E7m1&z7&-1z&h& -  hmj1vvemz,GH7m-.v.z-z11.m&.q, E1^Phz7,--?eE,? v&7.zve1h.%va1e1--?{hE zaaF,z1?zhm hm0vzze1rh?m?a-e8vz,,?&,?E-m &?7ehv&nv? aa,7--a,hvve1zh1-h11 ah.77_1,m1&fa  za.7h-z?v7&.&zhEv-1??hL 1a.Ezhh&1v-. a,ez-m?,h7-vz11-mE&^h-PzaEe-za&v7--e,v1?m-&-h1G?v+e1z.a&7m-9?0h&z,EEhmV,v7?E ,a77mem,1E7m1I?e.- ?&h& eaEaE-z,mE, ea,a-ze1Th,,vE&mz&vvmh-J,am7h&.77.1,.E&7 -z?mh.,,Evmm&,7e. ,?Ee &a%7.-&,Ev-.7z.1.m-,mE, 7& 7m-e?<E1;za?7E-z?aEm zav7m-,?,E  Eaee4-1?.h& Ev,ea-m?zhe ma?e7- 1ihelna1e-z&?Eh-lv7&ezzv?mh,&va e?ze1&h1V.v&.m,k1ahh8zvmemz,17 &)1ve.fz11-m&MEv-zz,a1zmv#mv .7,:1?mz&<v .-,&1Em-&zvh.,,v1mm &7v .?,.E#m1&.ev-x,-Eamh&?7v.m,,E7Fh&?7.-^,1E. 7&E---?,hE, vaa7,-?,  ?gmaM7E-.?hEE maa-hzE?vE  ,v&7 - ?ema&-a.ev-E1&ha hazevzz?,h-  a?eezH?1h.CmaEe,za? hz57am.1zh? hEkevze1z.1& Eavvae zz1ehm&av7e ?v1emv91v-.&zE1-m78 vz.-zm1,m7j v?.e? 11mz&&7_.-,v1hmz&hvm.,,7E8m?&e7C.1"); local n = 0; r.RC_TvLYg(function() r.FJFGw_qo() n = n + 1 end) local function e(l, e) if e then
+return n end; n = l + n; end local l, n, u = o(0, o, e, h, r.ulyffmmX); local function a() local l, n = r.ulyffmmX(h, e(1, 3), e(5, 6) + 2); e(2);
+return (n * 256) + l; end; local k = true; local k = 0 local function z() local e = n(); local n = n(); local t = 1; local d = (l(n, 1, 20) * (2 ^ 32)) + e; local e = l(n, 21, 31); local n = (
+(-1) ^ l(n, 32));
+if (e == 0) then
+if (d == k) then
+return n * 0;
+else e = 1; t = 0; end; elseif(e == 2047) then
+return (d == 0) and(n * (1 / 0)) or(n * (0 / 0)); end;
+return r.bvHCyhx_(n, e - 1023) * (t + (d / (2 ^ 52))); end; local b = n; local function y(n) local l;
+if (not n) then n = b();
+if (n == 0) then
+return ''; end; end; l = r.RWLBjcah(h, e(1, 3), e(5, 6) + n - 1); e(n) local e = ""
+for n = (1 + k), #l do e = e.r.RWLBjcah(l, n, n) end
+return e; end; local k = #r.KamTkDAW(s('\49.\48'))~ = 1 local e = n; local function _(.) return {
+    .
+},
+r.llSuxPvp('#', .) end local function p() local e = {}; local o = {}; local s = {}; local c = {
+    s,
+    o,
+    nil,
+    e
+}; local e = n() local h = {}
+for d = 1, e do local l = u(); local e;
+if (l == 2) then e = (u()~ = # {}); elseif(l == 3) then local n = z();
+if k and r.lXenWuAl(r.KamTkDAW(n), '.(\48+)$') then n = r.QU_LuBJv(n); end e = n; elseif(l == 1) then e = y(); end; h[d] = e; end;
+for e = 1, n() do o[e - (# {
+    1
+})] = p(); end;
+for c = 1, n() do local e = u();
+if (l(e, 1, 1) == 0) then local o = l(e, 2, 3); local r = l(e, 4, 6); local e = {
+    a(),
+    a(),
+    nil,
+    nil
+};
+if (o == 0) then e[t] = a(); e[f] = a(); elseif(o == # {
+    1
+}) then e[t] = n(); elseif(o == m[2]) then e[t] = n() - (2 ^ 16) elseif(o == m[3]) then e[t] = n() - (2 ^ 16) e[f] = a(); end;
+if (l(r, 1, 1) == 1) then e[d] = h[e[d]] end
+if (l(r, 2, 2) == 1) then e[t] = h[e[t]] end
+if (l(r, 3, 3) == 1) then e[f] = h[e[f]] end s[c] = e; end end; c[3] = u();
+return c; end; local function ee(l, n, e) local d = n; local d = e;
+return s(r.lXenWuAl(r.lXenWuAl(
+({
+    r.RC_TvLYg(l)
+})
+[2], n), e)) end local function z(m, e, h) local function ee(.) local a, s, u, p, j, l, ee, y, b, k, g, n; local e = 0;
+while - 1 < e do
+if 2 >= e then
+if e <= 0 then a = o(6, 47, 1, 30, m); s = o(6, 64, 2, 19, m);
+else
+if - 2 <= e then
+for n = 27, 73 do
+if e~ = 2 then u = o(6, 38, 3, 80, m); j = _ p = 0;
+break; end; l = -41; ee = -1;
+break; end;
+else u = o(6, 38, 3, 80, m); j = _ p = 0; end end
+else
+if 5 <= e then
+if e > 4 then repeat
+if 6 > e then n = o(7);
+break; end; e = -2; until true;
+else e = -2; end
+else
+if 0 <= e then repeat
+if 4 > e then y = {}; b = {
+    .
+};
+break; end; k = r.llSuxPvp('#', .) - 1; g = {}; until true;
+else k = r.llSuxPvp('#', .) - 1; g = {}; end end end e = e + 1; end;
+for e = 0, k do
+if (e >= u) then y[e - u] = b[e + 1];
+else n[e] = b[e + 1]; end; end; local e = k - u + 1 local e; local o; local function r(.) while true do end end
+while true do
+if l < -40 then l = l + 42 end e = a[l]; o = e[ne];
+if 15 >= o then
+if o <= 7 then
+if o < 4 then
+if o >= 2 then
+if 2 < o then n[e[d]] = h[e[t]];
+else n[e[d]] = z(s[e[t]], nil, h); end
+else
+if o~ = -2 then
+for r = 38, 84 do
+if 1~ = o then
+if (n[e[d]]~ = e[f]) then l = l + 1;
+else l = e[t]; end;
+break; end; l = e[t];
+break; end;
+else l = e[t]; end end
+else
+if o >= 6 then
+if 6~ = o then do
+return end;
+else do
+return end; end
+else
+if 0 <= o then repeat
+if o < 5 then local r, f, h, a, o; local l = 0;
+while l > -1 do
+if 3 > l then
+if l >= 1 then
+if - 1 <= l then
+for e = 29, 76 do
+if 2 > l then f = d;
+break; end; h = t;
+break; end;
+else h = t; end
+else r = e; end
+else
+if l <= 4 then
+if 0 < l then repeat
+if l > 3 then o = r[f];
+break; end; a = r[h]; until true;
+else o = r[f]; end
+else
+if 3~ = l then
+for e = 49, 60 do
+if 5 < l then l = -2;
+break; end; n(o, a);
+break; end;
+else n(o, a); end end end l = l + 1 end
+break; end; local o, h, a, r, f, c; local l = 0;
+while l > -1 do
+if 4 > l then
+if 2 > l then
+if l~ = -1 then
+for n = 29, 54 do
+if l~ = 0 then h = d;
+break; end; o = e;
+break; end;
+else h = d; end
+else
+if 3 == l then r = n;
+else a = t; end end
+else
+if l >= 6 then
+if l == 6 then n[c] = f;
+else l = -2; end
+else
+if l > 2 then
+for e = 47, 81 do
+if l < 5 then f = r[o[a]];
+break; end; c = o[h];
+break; end;
+else f = r[o[a]]; end end end l = l + 1 end until true;
+else local o, h, a, r, f, c; local l = 0;
+while l > -1 do
+if 4 > l then
+if 2 > l then
+if l~ = -1 then
+for n = 29, 54 do
+if l~ = 0 then h = d;
+break; end; o = e;
+break; end;
+else h = d; end
+else
+if 3 == l then r = n;
+else a = t; end end
+else
+if l >= 6 then
+if l == 6 then n[c] = f;
+else l = -2; end
+else
+if l > 2 then
+for e = 47, 81 do
+if l < 5 then f = r[o[a]];
+break; end; c = o[h];
+break; end;
+else f = r[o[a]]; end end end l = l + 1 end end end end
+else
+if 12 > o then
+if o < 10 then
+if o~ = 7 then repeat
+if 8~ = o then n[e[d]] = h[e[t]];
+break; end; h[e[t]] = n[e[d]]; until true;
+else h[e[t]] = n[e[d]]; end
+else
+if 7~ = o then
+for l = 46, 95 do
+if 11 > o then local l = e[d] n[l]
+(c(n, l + 1, e[t])) break; end; n[e[d]]
+[e[t]] = e[f];
+break; end;
+else n[e[d]]
+[e[t]] = e[f]; end end
+else
+if o >= 14 then
+if o~ = 15 then n[e[d]] = {};
+else
+for o = 0, 3 do
+if 2 <= o then
+if 1 < o then
+for r = 14, 56 do
+if o~ = 2 then
+if (n[e[d]]~ = e[f]) then l = l + 1;
+else l = e[t]; end;
+break; end; n[e[d]] = h[e[t]]; l = l + 1; e = a[l];
+break; end;
+else n[e[d]] = h[e[t]]; l = l + 1; e = a[l]; end
+else
+if - 2 < o then repeat
+if o~ = 0 then h[e[t]] = n[e[d]]; l = l + 1; e = a[l];
+break; end; n[e[d]] = (e[t]~ = 0); l = l + 1; e = a[l]; until true;
+else h[e[t]] = n[e[d]]; l = l + 1; e = a[l]; end end end end
+else
+if 11~ = o then repeat
+if o~ = 12 then l = e[t];
+break; end;
+if (n[e[d]]~ = e[f]) then l = l + 1;
+else l = e[t]; end; until true;
+else l = e[t]; end end end end
+else
+if o >= 24 then
+if 28 > o then
+if 26 <= o then
+if 25 <= o then
+for l = 36, 54 do
+if o~ = 26 then local l = e[d] n[l]
+(c(n, l + 1, e[t])) break; end; n(e[d], e[t]);
+break; end;
+else local l = e[d] n[l]
+(c(n, l + 1, e[t])) end
+else
+if 21 <= o then
+for l = 41, 56 do
+if 25 > o then n[e[d]] = n[e[t]]
+[e[f]];
+break; end; local e = e[d] n[e]
+(n[e + 1]) break; end;
+else local e = e[d] n[e]
+(n[e + 1]) end end
+else
+if o > 29 then
+if o~ = 31 then local c, k, z, m, s, u, o, r;
+for o = 0, 6 do
+if 2 >= o then
+if o < 1 then n[e[d]] = {}; l = l + 1; e = a[l];
+else
+if o > -3 then repeat
+if o~ = 2 then n[e[d]]
+[e[t]] = e[f]; l = l + 1; e = a[l];
+break; end; n[e[d]]
+[e[t]] = e[f]; l = l + 1; e = a[l]; until true;
+else n[e[d]]
+[e[t]] = e[f]; l = l + 1; e = a[l]; end end
+else
+if o < 5 then
+if 0 <= o then repeat
+if o > 3 then n[e[d]] = h[e[t]]; l = l + 1; e = a[l];
+break; end; n[e[d]]
+[e[t]] = e[f]; l = l + 1; e = a[l]; until true;
+else n[e[d]] = h[e[t]]; l = l + 1; e = a[l]; end
+else
+if o > 3 then
+for f = 30, 89 do
+if o~ = 6 then o = 0;
+while o > -1 do
+if o >= 4 then
+if o > 5 then
+if 2 <= o then
+for e = 11, 60 do
+if 6 < o then o = -2;
+break; end; n[u] = s;
+break; end;
+else n[u] = s; end
+else
+if 3~ = o then
+for e = 40, 91 do
+if 4 < o then u = c[k];
+break; end; s = m[c[z]];
+break; end;
+else u = c[k]; end end
+else
+if o >= 2 then
+if o == 2 then z = t;
+else m = n; end
+else
+if o == 1 then k = d;
+else c = e; end end end o = o + 1 end l = l + 1; e = a[l];
+break; end; r = e[d] n[r]
+(n[r + 1]) break; end;
+else r = e[d] n[r]
+(n[r + 1]) end end end end
+else local o; o = e[d] n[o]
+(c(n, o + 1, e[t])) l = l + 1; e = a[l]; do
+return end; end
+else
+if o > 26 then
+for r = 12, 73 do
+if 29~ = o then
+for o = 0, 1 do
+if 1 > o then n[e[d]] = n[e[t]]
+[e[f]]; l = l + 1; e = a[l];
+else n[e[d]] = n[e[t]]; end end
+break; end; n[e[d]] = z(s[e[t]], nil, h);
+break; end;
+else n[e[d]] = z(s[e[t]], nil, h); end end end
+else
+if 20 <= o then
+if o <= 21 then
+if 17 < o then
+for l = 30, 68 do
+if o~ = 21 then n[e[d]] = {};
+break; end; n[e[d]] = (e[t]~ = 0);
+break; end;
+else n[e[d]] = {}; end
+else
+if o~ = 23 then n[e[d]] = n[e[t]]
+[e[f]];
+else n[e[d]] = (e[t]~ = 0); end end
+else
+if o <= 17 then
+if 14 < o then
+for l = 36, 90 do
+if 16 < o then h[e[t]] = n[e[d]];
+break; end; n[e[d]] = n[e[t]];
+break; end;
+else n[e[d]] = n[e[t]]; end
+else
+if 16 <= o then
+for l = 17, 64 do
+if o~ = 19 then local e = e[d] n[e]
+(n[e + 1]) break; end; n[e[d]]
+[e[t]] = e[f];
+break; end;
+else n[e[d]]
+[e[t]] = e[f]; end end end end end l = 1 + l; end; end;
+return ee end; local d = 0xff; local h = {}; local f = (1); local t = '';
+(function(n) local l = n local o = 0x00 local e = 0x00 l = {
+    (function(r) if o > 0x1e then
+    return r end o = o + 1 e = (e + 0x44f - r) % 0x12
+    return (e % 0x03 == 0x1 and(function(l) if not n[l] then e = e + 0x01 n[l] = (0x8b); end
+    return true end)'JSSGU'
+    and l[0x3]
+    (0x6c + r)) or(e % 0x03 == 0x0 and(function(l) if not n[l] then e = e + 0x01 n[l] = (0xdb); end
+    return true end)'YHduT'
+    and l[0x1]
+    (r + 0x175)) or(e % 0x03 == 0x2 and(function(l) if not n[l] then e = e + 0x01 n[l] = (0x96); d[2] = (d[2] * (ee(function() h() end, c(t)) - ee(d[1], c(t)))) + 1; h[f] = {}; d = d[2]; f = f + d; end
+    return true end)'xnYRE'
+    and l[0x2]
+    (r + 0x164)) or r end),
+    (function(r) if o > 0x24 then
+    return r end o = o + 1 e = (e + 0xd0c - r) % 0x43
+    return (e % 0x03 == 0x1 and(function(l) if not n[l] then e = e + 0x01 n[l] = (0x6b); t = '\37'; d = {
+        function() d() end
+    }; t = t.'\100\43'; end
+    return true end)'ouhpy'
+    and l[0x3]
+    (0x203 + r)) or(e % 0x03 == 0x2 and(function(l) if not n[l] then e = e + 0x01 n[l] = (0x92); end
+    return true end)'vhbbo'
+    and l[0x2]
+    (r + 0x1fc)) or(e % 0x03 == 0x0 and(function(l) if not n[l] then e = e + 0x01 n[l] = (0x29); h[f] = le(); f = f + d; end
+    return true end)'zxtZM'
+    and l[0x1]
+    (r + 0x32a)) or r end),
+    (function(a) if o > 0x27 then
+    return a end o = o + 1 e = (e + 0x437 - a) % 0x4f
+    return (e % 0x03 == 0x0 and(function(l) if not n[l] then e = e + 0x01 n[l] = (0xdb); end
+    return true end)'kvffO'
+    and l[0x3]
+    (0x22b + a)) or(e % 0x03 == 0x1 and(function(l) if not n[l] then e = e + 0x01 n[l] = (0x3a); t = {
+        t.'\58 a',
+        t
+    }; h[f] = p(); f = f + (
+    (not r.UFEySHre) and 1 or 0); t[1] = '\58'.t[1]; d[2] = 0xff; end
+    return true end)'muTOv'
+    and l[0x1]
+    (a + 0x101)) or(e % 0x03 == 0x2 and(function(l) if not n[l] then e = e + 0x01 n[l] = (0xe); end
+    return true end)'rZMxa'
+    and l[0x2]
+    (a + 0x9c)) or a end)
+}
+l[0x2]
+(0x3e2) end) {}; local e = z(c(h));
+return e(.); end
+return y(
+(function() local n = {}
+local e = 0x01; local l;
+if r.UFEySHre then l = r.UFEySHre(y)
+else l = ''
+end
+if r.lXenWuAl(l, r.GjmSk_PM) then e = e + 0;
+else e = e + 1; end n[e] = 0x02; n[n[e] + 0x01] = 0x03;
+return n; end)
+(), .) end)
+(
+(function(e, n, l, d, t, o) local o;
+if 4 <= e then
+if e > 5 then
+if e < 7 then do
+return t[l] end;
+else
+if e~ = 7 then do
+return l(e, nil, l); end
+else do
+return setmetatable({},
+{
+    ['__\99\97\108\108'] = function(e, d, l, t, n) if n then
+    return e[n] elseif t then
+    return e
+    else e[d] = l end end
+}) end end end
+else
+if e == 5 then local e = d; do
+return function() local n = n(l, e(e, e), e(e, e)); e(1);
+return n; end; end;
+else local e = d; local d, t, o = t(2); do
+return function() local r, n, l, f = n(l, e(e, e), e(e, e) + 3); e(4);
+return (f * d) + (l * t) + (n * o) + r; end; end; end end
+else
+if 1 >= e then
+if e == 0 then do
+return n(1), n(4, t, d, l, n), n(5, t, d, l) end;
+else do
+return function(l, e, n) if n then local e = (l / 2 ^ (e - 1)) % 2 ^ (
+(n - 1) - (e - 1) + 1);
+return e - e % 1;
+else local e = 2 ^ (e - 1);
+return (l % (e + e) >= e) and 1 or 0; end; end; end; end
+else
+if e >= 0 then
+for o = 33, 56 do
+if 2 < e then do
+return n(1), n(4, t, d, l, n), n(5, t, d, l) end;
+break; end; do
+return 16777216, 65536, 256 end;
+break; end;
+else do
+return 16777216, 65536, 256 end; end end end end), .)
