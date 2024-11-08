522be50f23b2fca1bf77e565d33dd1949102c801
HoangWhites <hoangnamwhiteshub@gmail.com>
2024-11-08 06:55:33 -0800

    Update Hookfunction.lua

--- Hookfunction.lua
+++ Hookfunction.lua
@@ -1,9 +1,7 @@
 if hookfunction and not islclosure(hookfunction) then 
-    for i,v in game:GetService("ReplicatedStorage").Assets.GUI:GetChildren() do 
+    for i, v in next, RS.Assets.GUI:GetChildren() do 
         v.Enabled = false 
     end
-    if not fluxus then
-        hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death), function() end)
-        hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Respawn), function() end)
-    end
+    hookfunction(require(RS.Effect.Container.Death), function() end)
+    hookfunction(require(RS.Effect.Container.Respawn), function() end)
 end
