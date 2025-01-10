e878fac58bd02f92864e7eba18b63ecd66c30226
HoangWhites <hoangnamwhiteshub@gmail.com>
2025-01-10 01:20:46 -0800

    Update Hookfunction.lua

--- Hookfunction.lua
+++ Hookfunction.lua
@@ -1,5 +1,3 @@
-for i, v in pairs(game.ReplicatedStorage.Assets.GUI:GetChildren()) do 
-    v.Enabled = false 
-end
+require(game.ReplicatedStorage.Util.CameraShaker):Stop()
 hookfunction(require(game.ReplicatedStorage.Effect.Container.Death), function() end)
 hookfunction(require(game.ReplicatedStorage.Effect.Container.Respawn), function() end)
